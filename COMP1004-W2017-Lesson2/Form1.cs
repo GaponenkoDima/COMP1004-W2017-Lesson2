dfe5dda39a4b386a6357b6056a1eaa245a0c529f
dmytrogaponenko <dmytrogaponenko@DMYTROGAPON6FBC>
2017-01-23 12:37:19 -0500

    Add project files.

---
+++ COMP1004-W2017-Lesson2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_W2017_Lesson2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            Button CalculatorButton = sender as Button;
+
+            ResultTextBox.Text += CalculatorButton.Text;
+        }
+    }
+}
